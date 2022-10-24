e989cf73188815f242aa82eef4b014f629ea32e9
Daniel Dominguez Dominguez <danarturodo@hotmail.com>
2022-10-23 23:00:58 -0500

    se modificaron los routing

--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../cliente.module';
+import { ClienteService } from '../cliente.service';
+import { ServicioEmpleadosService } from '../servicio-empleados.service';
 
 @Component({
   selector: 'app-home-component',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-component.component.css']
 })
 export class HomeComponentComponent implements OnInit {
-
-  constructor() { }
+  titulo = 'Listado de clientes';
+  mensaje = '';
+  registrado =false;
+  cargo:string="";
+  nombre = '';
+  apellido = '';
+  constructor(private service:ServicioEmpleadosService,
+    private clienteService:ClienteService) { }
 
   ngOnInit(): void {
+    this.clientes=this.clienteService.clientes;
+  }
+  clientes:Cliente[]=[];
+
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:any="00";
+  agregarCliente(){
+    let miCliente = new Cliente(this.cuadroNombre , this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+    // this.service.muestraMensaje("Nombre del empleado"
+    // + miCliente.nombre);
+    this.clienteService.agregarClienteServicio(miCliente)
   }
 
+  arrayCaracteristicas = [''];
+
+  addCaracteristicas(newCaracteristica: string) {
+    this.arrayCaracteristicas.push(newCaracteristica);
+  }
 }
